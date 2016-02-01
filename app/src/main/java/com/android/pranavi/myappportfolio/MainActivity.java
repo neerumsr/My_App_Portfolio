fed629a650d476dfcad5bc5e7b04b57c066c2f83
Siva Mupparaju <msrprasad@gmail.com>
2016-02-01 11:50:14 -0800

    Updated logic

--- app/src/main/java/com/android/pranavi/myappportfolio/MainActivity.java
+++ app/src/main/java/com/android/pranavi/myappportfolio/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +20,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +42,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void buttonClickEvent(View view)
+    {
+        switch(view.getId()) {
+            case R.id.button_spotify:
+                Toast.makeText(this,"This would open Spotify App",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.button_scores:
+                Toast.makeText(this, "This would open Scores App", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_library:
+                Toast.makeText(this, "This would open Library App", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_build:
+                Toast.makeText(this, "This would open Build it Bigger App", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_xyz:
+                Toast.makeText(this, "This would open XYZ reader App", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_capstone:
+                Toast.makeText(this, "This would open Capstone App", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this,"Work in progress",Toast.LENGTH_SHORT).show();
+        }
     }
 }
